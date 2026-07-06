69159e4b32e8e0c3c86b6de354e1935100f3bf6d
Anik kumar <148265561+anik-ug@users.noreply.github.com>
2026-07-06 21:37:35 +0530

    Time: 20 ms (5.09%) | Memory: 46.6 MB (24.23%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.java
+++ 1222-remove-covered-intervals/remove-covered-intervals.java
@@ -3,20 +3,21 @@ class Solution {
         int n = intervals.length;
         
         boolean[] covered = new boolean[n];
-        Arrays.sort(intervals,(a,b)->{
-            if(a[0]==b[0])return Integer.compare(b[1],a[1]);
 
-            return Integer.compare(a[0],b[0]); 
+        Arrays.sort(intervals,(a,b)->{
+            if(a[0]==b[0])return Integer.compare(a[1],b[1]);
+
+            return Integer.compare(b[0],a[0]); 
             });
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-1;i++){
             int a = intervals[i][0];
             int b = intervals[i][1];
             for(int j=i+1;j<n;j++){
                 
                 int c = intervals[j][0];
                 int d = intervals[j][1];
-                if(a <= c && d <= b){
-                    covered[j]=true;
+                if(c <= a && b <= d){
+                    covered[i]=true;
                 }
                 
             }
