d2f2858c3dfbb3aca2e91a4556126d8235a471ea
Anik kumar <148265561+anik-ug@users.noreply.github.com>
2026-08-18 22:45:59 +0530

    Time: 1 ms (78.00%) | Memory: 10 MB (94.21%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isSafe(int n,vector<string>& nQueen,int row,int col){
+        for(int i=0;i<n;i++){
+            if(nQueen[i][col]=='Q'){
+                return false;
+            }
+        }
+        for(int i=row-1 , j=col-1;i>=0 && j>=0;i--,j--){
+            if(nQueen[i][j]=='Q'){
+                return false;
+            }
+        }
+        for(int i=row-1,j=col+1;i>=0 && j>=0;i--,j++){
+            if(nQueen[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    void solve(int n,vector<vector<string>>& output,vector<string>& nQueen,int row){
+        if(row==n){
+            output.push_back(nQueen);
+            return;
+        }
+
+        for(int col=0; col<n; col++){
+            if(isSafe(n,nQueen,row,col)){
+                nQueen[row][col] = 'Q';
+                solve(n,output,nQueen,row+1);
+                nQueen[row][col] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>output;
+        vector<string>nQueen(n,string(n,'.'));
+        solve(n,output,nQueen,0);
+        return output;
+    }
+};
