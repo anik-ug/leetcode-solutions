337ba192311993994a61dade5aeab8742d0e63ed
Anik kumar <148265561+anik-ug@users.noreply.github.com>
2026-08-24 01:06:19 +0530

    Time: 7 ms (60.96%) | Memory: 13 MB (80.80%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -5,7 +5,7 @@ public:
         if(i<0)return j+1;
         if(j<0)return i+1;
         if(dp[i][j]!=-1)return dp[i][j];
-        if(s1[i]==s2[j])return solve(i-1,j-1,s1,s2,dp);
+        if(s1[i]==s2[j])return dp[i][j] = solve(i-1,j-1,s1,s2,dp);
         return dp[i][j] = 1+min(solve(i-1,j,s1,s2,dp),min(solve(i-1,j-1,s1,s2,dp),solve(i,j-1,s1,s2,dp)));
     }
     int minDistance(string word1, string word2) {
