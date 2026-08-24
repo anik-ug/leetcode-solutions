9e64565f2107caa9867c5c3e7e27a21f7ef95ef8
Anik kumar <148265561+anik-ug@users.noreply.github.com>
2026-08-25 03:49:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (97.33%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void solve(vector<vector<int>>& ans,vector<int>& v,vector<int>& nums,int start){
+        ans.push_back(v);
+        for(int i=start;i<nums.size();i++){
+            if(i>start && nums[i]==nums[i-1])continue;
+            v.push_back(nums[i]);
+            solve(ans,v,nums,i+1);
+            v.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        vector<int>v;
+        solve(ans,v,nums,0);
+        return ans;
+    }
+};
