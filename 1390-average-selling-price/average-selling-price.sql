506231c9e6c94e9117c4cb2daf4d0fff282f20d7
Anik kumar <148265561+anik-ug@users.noreply.github.com>
2026-09-20 15:19:28 +0530

    Time: 1136 ms (15.62%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT p.product_id,COALESCE(ROUND(SUM(p.price*units)/SUM(u.units),2),0) AS average_price
+SELECT p.product_id,IFNULL(ROUND(SUM(p.price*units)/SUM(u.units),2),0) AS average_price
 FROM Prices p
 LEFT JOIN UnitsSold u
     ON p.product_id = u.product_id
